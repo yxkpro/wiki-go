c7f094a82bdf352c0859e51ea2d6a3c0fd208b50
Amin Persia <leomoon@gmail.com>
2025-05-24 14:18:32 -0700

    Adds timestamp to "dev" version string so static files are not cached when testing This will be overriden when building a release with the version number

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,17 @@
 package version
 
-var Version = "dev" // overridden at build time
+import (
+	"fmt"
+	"time"
+)
+
+// Version is set at build time via ldflags
+var Version = "dev"
+
+// init adds timestamp to dev builds to prevent caching
+func init() {
+	// Only add timestamp to dev builds
+	if Version == "dev" {
+		Version = fmt.Sprintf("dev-%s", time.Now().Format("20060102-150405"))
+	}
+}
